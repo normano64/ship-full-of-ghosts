c9ef1409cd38d6e15a4105ab79cd9099872a13db
Lei Mingyu <uygnim.iel@gmail.com>
2016-02-16 14:31:33 +0100

    sass task added in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+
+var paths = {
+  sass: 'app/sass/**/*.scss',
+  js: 'app/js/**/*.js'
+};
 
 gulp.task('serve', function() {
   gulp.src('app')
@@ -7,4 +13,14 @@ gulp.task('serve', function() {
         liveload: true,
         open: true
       }));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch(paths.sass, ['sass']);
 });
