dd7abd026085d1110dcf40c0cf8b2403b13c1864
Lei Mingyu <uygnim.iel@gmail.com>
2016-02-16 15:46:02 +0100

    login form

--- app/js/controllers/LoginCtrl.js
+++ app/js/controllers/LoginCtrl.js
@@ -3,5 +3,7 @@
 angular
   .module('shipFullOfGhosts.controllers')
   .controller('LoginCtrl', ['$scope', 'AccountSvc', function($scope, AccountSvc) {
-    
+    $scope.signin = function(username, password) {
+      console.log(username + ' has signed in!');
+    };
   }]);
