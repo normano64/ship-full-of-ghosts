8302a0e3905b301ce02fbee9b96012b8e693eaaa
Lei Mingyu <uygnim.iel@gmail.com>
2016-03-01 00:54:59 +0100

    controller for the cart

---
+++ app/js/controllers/CartCtrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular
+  .module('shipFullOfGhosts.controllers')
+  .controller('CartCtrl', ['$scope', 'AccountSvc', function($scope, AccountSvc) {
+    $scope.isExpanded = false;
+    $scope.expandText = '◀';
+
+    $scope.toggleCart = function() {
+      $scope.isExpanded = !$scope.isExpanded;
+      $scope.expandText = $scope.isExpanded ? '◀' : '▶';
+    }
+
+    $scope.user = AccountSvc.getUser();
+  }]);
