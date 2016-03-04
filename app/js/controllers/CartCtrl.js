fbcda6407ab1bb0e781bd65c037b3b85d67b7422
Lei Mingyu <uygnim.iel@gmail.com>
2016-03-04 22:36:14 +0100

    fake item in cart added

--- app/js/controllers/CartCtrl.js
+++ app/js/controllers/CartCtrl.js
@@ -6,6 +6,39 @@ angular
     $scope.isExpanded = false;
     $scope.expandText = '◀';
 
+    $scope.items = [
+      {
+        id: 0,
+        name: 'Brooklyn',
+        price: 79,
+        quantity: 1
+      },
+      {
+        id: 1,
+        name: 'Chimay blå',
+        price: 27.9,
+        quantity: 3
+      },
+      {
+        id: 2,
+        name: 'Thai',
+        price: 57,
+        quantity: 1
+      },
+      {
+        id: 3,
+        name: 'Kyckling Med',
+        price: 420,
+        quantity: 2
+      },
+      {
+        id: 4,
+        name: 'Thai Broileri',
+        price: 10,
+        quantity: 1
+      }
+    ];
+
     $scope.toggleCart = function() {
       $scope.isExpanded = !$scope.isExpanded;
       $scope.expandText = $scope.isExpanded ? '▶' : '◀';
