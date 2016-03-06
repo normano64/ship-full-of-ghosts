51645902d418febbcc493371e4baf0428d0de010
Normano64 <per.bergqwist@gmail.com>
2016-03-06 23:32:53 +0100

    Concat styles and scripts to single file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
+var concat = require('gulp-concat');
 
 var paths = {
   sass: 'app/sass/**/*.scss',
-  css: 'app/css/**/*.css',
-  js: 'app/js/**/*.js',
+  css: 'app/static/**/*.css',
+  js: 'app/static/**/*.js',
   index: 'app/index.html'
 };
 
@@ -14,7 +15,7 @@ gulp.task('serve', function() {
   gulp.src('app')
     .pipe(webserver({
         liveload: true,
-        open: true,
+        open: false,
         port: 8081
       }));
 });
@@ -22,7 +23,8 @@ gulp.task('serve', function() {
 gulp.task('sass', function() {
   return gulp.src(paths.sass)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('app/static'));
 });
 
 gulp.task('sass:watch', function() {
@@ -31,6 +33,9 @@ gulp.task('sass:watch', function() {
 
 gulp.task('index', function() {
   var target = gulp.src(paths.index);
+  var js = gulp.src(['app/js/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./app/static/'));
   var sources = gulp.src([paths.js, paths.css], {read: false});
 
   return target.pipe(inject(sources, {relative: true}))
