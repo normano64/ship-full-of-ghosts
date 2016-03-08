d0be3b84a6702c8f17e4b72b18b1da77ab5a7227
Lei Mingyu <uygnim.iel@gmail.com>
2016-03-08 16:50:58 +0100

    expand the cart after adding drinks

--- app/js/controllers/CartCtrl.js
+++ app/js/controllers/CartCtrl.js
@@ -3,13 +3,12 @@
 angular
   .module('shipFullOfGhosts.controllers')
   .controller('CartCtrl', ['$scope', 'AccountSvc', 'CartSvc', function($scope, AccountSvc, CartSvc) {
-    $scope.isExpanded = false;
     $scope.expandText = '◀';
 
-    $scope.items = CartSvc.getCart();
+    $scope.cart = CartSvc.getCart();
 
     $scope.toggleCart = function() {
-      $scope.isExpanded = !$scope.isExpanded;
+      $scope.cart.isExpanded = !$scope.cart.isExpanded;
       $scope.expandText = $scope.isExpanded ? '▶' : '◀';
     };
 
--- app/js/services/CartSvc.js
+++ app/js/services/CartSvc.js
@@ -12,6 +12,10 @@ angular
       });
 
     CartSvc.cart = {
+      items: {
+
+      },
+      isExpanded: false
     };
 
     CartSvc.getCart = function() {
@@ -19,31 +23,31 @@ angular
     };
 
     CartSvc.increaseItem = function(id) {
-      if (typeof CartSvc.cart[id] === 'undefined') {
+      if (typeof CartSvc.cart.items[id] === 'undefined') {
         console.log('something goes wrong, no changing anything...');
       } else {
-        CartSvc.cart[id].quantity ++;
+        CartSvc.cart.items[id].quantity ++;
       }
     };
 
     CartSvc.decreaseItem = function(id) {
-      if (typeof CartSvc.cart[id] === 'undefined') {
+      if (typeof CartSvc.cart.items[id] === 'undefined') {
         console.log('something goes wrong, no changing anything...');
       } else {
-        if (!(-- CartSvc.cart[id].quantity)) {
+        if (!(-- CartSvc.cart.items[id].quantity)) {
           // remove it from the cart
-          CartSvc.cart[id] = undefined;
+          CartSvc.cart.items[id] = undefined;
         }
       }
     };
 
     CartSvc.addItem = function(id) {
-      if (typeof CartSvc.cart[id] === 'undefined') {
+      if (typeof CartSvc.cart.items[id] === 'undefined') {
         var itemFound = false;
         for (var i = 0; i < allItems.length; i ++) {
           var item = allItems[i];
           if (item.beer_id === id) {
-            CartSvc.cart[id] = {
+            CartSvc.cart.items[id] = {
               beer_id: id,
               namn: item.namn,
               price: item.pub_price,
@@ -57,8 +61,10 @@ angular
           console.log('something goes wrong, no changing anything...');
         }
       } else {
-        CartSvc.cart[id].quantity ++;
+        CartSvc.cart.items[id].quantity ++;
       }
+
+      CartSvc.cart.isExpanded = true;
     };
 
     return CartSvc;
