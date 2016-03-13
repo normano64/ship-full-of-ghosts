d164d3fc7ffeb5318fcdebcf30a3e32af3af711f
Joakim Nilsson <jocke@Joakims-MacBook-Air.local>
2016-03-13 14:39:24 +0100

    translation now works as intended with same language state for all pages

--- app/js/controllers/SigninCtrl.js
+++ app/js/controllers/SigninCtrl.js
@@ -2,9 +2,10 @@
 
 angular
   .module('shipFullOfGhosts.controllers')
-  .controller('SigninCtrl', ['$scope', 'AccountSvc', function($scope, AccountSvc) {
+  .controller('SigninCtrl', ['$scope', 'AccountSvc', 'TranslateSvc', function($scope, AccountSvc, TranslateSvc) {
     $scope.signIn = function(username, password) {
       AccountSvc.signIn(username, password);
       console.log(username + ' has signed in!');
     };
+    $scope.wordsList = TranslateSvc.wordsList;
   }]);
