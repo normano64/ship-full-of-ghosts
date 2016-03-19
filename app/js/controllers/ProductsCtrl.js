8304105ad0dd856ff55112739b412a314cdd5345
Normano64 <per.bergqwist@gmail.com>
2016-03-19 17:45:44 +0100

    Moved around code

--- app/js/controllers/ProductsCtrl.js
+++ app/js/controllers/ProductsCtrl.js
@@ -72,19 +72,38 @@ angular
           return angular.element(document.querySelectorAll(identifier));
         };
 
+          var cartElement = selectElement('.cart');
+          var cartPhantomElement = selectElement('.cart-phantom');
+          var wrapperElement = selectElement('.product-wrapper');
+          var cartDropWrapperElement = selectElement('.cart-drop-wrapper');
+          var cartDropIconElement = selectElement('.cart-drop-icon');
+
+          var initialOffset = parseInt(cartElement.css('margin-top'));
+          cartDropIconElement.bind('drop', function(ev) {
+              var selectedId = ev.originalEvent.dataTransfer.getData('id');
+              CartSvc.addItem(selectedId);
+          });
+
+          cartDropIconElement.bind('dragover', function() {
+              $scope.canDrop = true;
+              $scope.$apply();
+              return false;
+          });
+
+          cartDropIconElement.bind('dragleave', function() {
+              $scope.canDrop = false;
+              $scope.$apply();
+              return false;
+          });
+
+          angular.element($window).bind("scroll", function() {
+              cartPhantomElement.css('margin-top', initialOffset + this.pageYOffset + 'px');
+          });
           $scope.$watch(function() {
               return selectElement('.products-items')[0].childNodes.length;
           }, function (newValue, oldValue) {
               if (newValue > oldValue) {
-                  var cartElement = selectElement('.cart');
-                  var cartPhantomElement = selectElement('.cart-phantom');
                   var productsItemElement = selectElement('.products-item');
-                  var wrapperElement = selectElement('.product-wrapper');
-                  var cartDropWrapperElement = selectElement('.cart-drop-wrapper');
-                  var cartDropIconElement = selectElement('.cart-drop-icon');
-
-                  var initialOffset = parseInt(cartElement.css('margin-top'));
-
                   productsItemElement.bind('dragstart', function(ev) {
                       ev.originalEvent.dataTransfer.setData('id', angular.element(this).attr('id').slice('products-item-'.length));
 
@@ -108,27 +127,6 @@ angular
 
                       productsItemElement.css('opacity', '1');
                       cartDropWrapperElement.css('display', 'none');
-                  });
-
-                  cartDropIconElement.bind('drop', function(ev) {
-                      var selectedId = ev.originalEvent.dataTransfer.getData('id');
-                      CartSvc.addItem(selectedId);
-                  });
-
-                  cartDropIconElement.bind('dragover', function() {
-                      $scope.canDrop = true;
-                      $scope.$apply();
-                      return false;
-                  });
-
-                  cartDropIconElement.bind('dragleave', function() {
-                      $scope.canDrop = false;
-                      $scope.$apply();
-                      return false;
-                  });
-
-                  angular.element($window).bind("scroll", function() {
-                      cartPhantomElement.css('margin-top', initialOffset + this.pageYOffset + 'px');
                   });
               }
           });
